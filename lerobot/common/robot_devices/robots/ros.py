124d9cfafcdd7c9d9b34f27d7ed2a89c2e524b9b
james <dirkmcpherson@gmail.com>
2024-09-06 10:48:50 -0400

    stubbed out ros robot class

---
+++ lerobot/common/robot_devices/robots/ros.py
@@ -0,0 +1,60 @@
+from .utils import Robot
+from lerobot.common.robot_devices.cameras.utils import Camera
+from dataclasses import dataclass, field, replace
+import torch
+
+@dataclass
+class RosRobotConfig:
+        cameras: dict[str, Camera] = field(default_factory=lambda: {})
+
+
+class RosRobot(Robot):
+    def __init__(self,
+                 config: RosRobotConfig | None = None,
+                 **kwargs) -> None:
+        super().__init__()
+        if config is None:
+            config = RosRobotConfig()
+        # Overwrite config arguments using kwargs
+        self.config = replace(config, **kwargs)
+
+        self.cameras = self.config.cameras
+        self.is_connected = False
+        self.logs = {}
+
+    def init_teleop(self): 
+        raise NotImplementedError
+
+    def run_calibration(self): 
+        # TODO: Home arm or reset arm?
+        raise NotImplementedError
+
+    def teleop_step(self, record_data=False):
+
+        # Grab the teleoperation data and send it using ::send_action
+        action = None
+
+        if not record_data: return
+
+        obs_dict = self.capture_observation()
+
+        action_dict = {'action': torch.from_numpy(action)}
+
+        raise NotImplementedError
+
+    def capture_observation(self): 
+        raise NotImplementedError
+        # Populate output dictionnaries and format to pytorch
+        obs_dict = {}
+        obs_dict["observation.state"] = torch.from_numpy(state)
+        for name in self.cameras:
+            obs_dict[f"observation.images.{name}"] = torch.from_numpy(images[name])
+        return obs_dict
+
+
+    def send_action(self, action):
+        raise NotImplementedError
+        success = False
+        # Command the robot to take the action
+        return success
+
