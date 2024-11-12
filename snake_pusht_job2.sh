693e4a07b7e5203ea8890d2e9f2652d0662c0927
j <dirkmcpherson@gmail.com>
2024-11-12 12:40:43 -0500

    switchover to diffusion

--- snake_pusht_job2.sh
+++ snake_pusht_job2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J snake_diffusion_p100 #job name
+#SBATCH -J snake_diffusion_a100 #job name
 #SBATCH --time=03-00:00:00 #requested time (DD-HH:MM:SS)
 #SBATCH -p gpu #running on "mpi" partition/queue
-#SBATCH --gres=gpu:p100:1 #requesting 1 GPU
-#SBATCH --constraint="p100"
+#SBATCH --gres=gpu:a100:1 #requesting 1 GPU
+#SBATCH --constraint="a100-80G"
 #SBATCH -N 1 #1 nodes
 #SBATCH -n 8 #2 tasks total (default 1 CPU core per task) = # of cores
 #SBATCH --mem=64g #RAM total
@@ -30,7 +30,7 @@ source activate three_ten
 
 # python lerobot/scripts/train.py policy=vqbet env=pusht dataset_repo_id='pusht/13_96x96' hydra.job.name='imi13_96' seed=423213 wandb.project='vqbet_pusht'
 # python lerobot/scripts/train.py policy=vqbet env=pusht dataset_repo_id='pusht/A13_96x96' hydra.job.name='Aimi13_96' seed=423213 wandb.project='vqbet_pusht'
-DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='12out' hydra.job.name='12out' seed=43 wandb.project='diffusion_pusht'
-DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='13out' hydra.job.name='13out' seed=13 wandb.project='diffusion_pusht'
+# DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='12out' hydra.job.name='12out' seed=43 wandb.project='diffusion_pusht'
+# DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='13out' hydra.job.name='13out' seed=13 wandb.project='diffusion_pusht'
 DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='14out' hydra.job.name='14out' seed=21 wandb.project='diffusion_pusht'
 DATA_DIR='./local/models' python lerobot/scripts/train.py policy=diffusion env=pusht dataset_repo_id='11out' hydra.job.name='11out' seed=4213 wandb.project='diffusion_pusht'
